dcf2f39b2704c7bfae9620fa304165fc4bb1d7b7
Eric <wenkuan.is.me@gmail.com>
2026-06-21 14:43:41 +0800

    feat: 新增Notification 寄信功能

--- src/main/java/com/eric/eBank/EBankApplication.java
+++ src/main/java/com/eric/eBank/EBankApplication.java
@@ -1,16 +1,37 @@
 package com.eric.eBank;
 
+import com.eric.eBank.auth_users.entity.User;
+import com.eric.eBank.notification.dtos.NotificationDTO;
+import com.eric.eBank.notification.entity.Notification;
+import com.eric.eBank.notification.services.NotificationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
+@RequiredArgsConstructor
 public class EBankApplication {
+
+	private final NotificationService notificationService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(EBankApplication.class, args);
 	}
 
+//	@Bean
+//    CommandLineRunner runner(){
+//		return args -> {
+//            NotificationDTO notificationDTO = NotificationDTO.builder()
+//					.recipient("eric40518@yahoo.com.tw")
+//					.subject("Test Notification")
+//					.body("This is a test notification.")
+//					.build();
+//            notificationService.sendEmail(notificationDTO, new User());
+//        };
+//    }
 }
--- src/main/java/com/eric/eBank/enums/NotificationType.java
+++ src/main/java/com/eric/eBank/enums/NotificationType.java
@@ -1,6 +1,5 @@
 package com.eric.eBank.enums;
 
 public enum NotificationType {
-    // EMAIL, SMS, PUSH
-    電子郵件, 簡訊, 推播通知
+    EMAIL, SMS, PUSH
 }
